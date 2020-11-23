4af8c762f43a76ee7baa7d2a4b9b14451bb21a00
AlexandrePdv <48692213+AlexandrePdv@users.noreply.github.com>
2020-11-23 02:19:29 +0100

    NavBar et Reseaux

--- client/components/Reseaux.vue
+++ client/components/Reseaux.vue
@@ -1,100 +1,119 @@
 <template>
-  <div>
-    <div>
-      <div class="py-12 bg-white">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="lg:text-center">
-            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Me suivre sur les réseaux
-            </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Suivez moi sur les réseaux sociaux pour être au courant en avant première des annonces et être notfiés des steams.
-            </p>
-          </div>
+<div class="min-h-screen min-w-screen py-16 bg-gray-900 text-gray-300">
+            <div class="lg:text-center items-center">
+                <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">
+                    Me suivre sur les réseaux
+                </p>
+                <img class="mt-4 max-w-2xl lg:mx-auto" src="https://pbs.twimg.com/profile_images/1315682895303004160/-_N5r_og_400x400.jpg" alt="">
+                <p class="mt-4 max-w-2xl text-xl lg:mx-auto">
+                    Suivez moi sur les réseaux sociaux pour être au courant en avant première des annonces et être notfiés des steams.
+                </p>
+            </div>
+            <div class="max-w-7xl mx-auto py-5 px-4 sm:px-6 lg:px-8">
+                <div class="mt-10">
+                <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    <div class="flex">
+                    <div class="flex-shrink-0">
+                        <div class="flex items-center justify-center h-12 w-12 rounded-md text-white">
+                            <img src="./twitch.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="ml-4">
+                        <dt class="text-lg leading-6 font-medium">
+                        Me suivre sur Twitch
+                        </dt>
+                        <span>
+                            Suivez moi sur 
+                            <a href="https://www.twitch.tv/mistermv" target="_blank" class="mt-2 text-base text-indigo-600">
+                            ma chaine Twitch.
+                            </a>
+                            Vous pourrez me voir galérer et m'entendre dire des conneries. Si vous êtes sages on commencera la prod de l'album.
+                        </span>
+                    </div>
+                    </div>
 
-          <div class="mt-10">
-            <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    Me suivre sur twitch
-                  </dt>
-                  <a href="https://www.twitch.tv/mistermv" target="_blank" class="mt-2 text-base text-gray-500">
-                    https://www.twitch.tv/mistermv
-                  </a>
-                </div>
-              </div>
+                    <div class="flex">
+                    <div class="flex-shrink-0">
+                        <div class="flex items-center justify-center h-12 w-12 rounded-md text-white">
+                            <img src="./youtube.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="ml-4">
+                        <dt class="text-lg leading-6 font-medium ">
+                        Ma Chaine Youtube
+                        </dt>
+                        <span>
+                            Sur 
+                            <a href="https://twitter.com/mistermv" target="_blank" class="mt-2 text-base text-indigo-600">
+                                la chaine Youtube
+                            </a>
+                            vous pourrez retrouver les redifs de mes lives Twitch. Plus d'excuses pour ne pas avoir les références de mes bêtises.
+                        </span>
+                    </div>
+                    </div>
 
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: scale -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    Ma Chaine Youtube
-                  </dt>
-                  <a href="https://twitter.com/mistermv" target="_blank" class="mt-2 text-base text-gray-500">
-                    https://www.youtube.com/user/monsieurmv
-                  </a>
-                </div>
-              </div>
+                    <div class="flex">
+                    <div class="flex-shrink-0">
+                        <div class="flex items-center justify-center h-12 w-12 rounded-md text-white">
+                            <img src="./soundcloud.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="ml-4">
+                        <dt class="text-lg leading-6 font-medium">
+                            Ecouter sur Soundcloud
+                        </dt>
+                        <span>
+                            Allez écouter mes vieux sons sur
+                            <a href="https://soundcloud.com/xavierdang" target="_blank" class="mt-2 text-base text-indigo-600">
+                                Soundcloud.
+                            </a>
+                            Qui sait, peut être qu'un jour vous pourrez retrouver le plus gros Album de tous les temps.
+                        </span>
+                    </div>
+                    </div>
 
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: lightning-bolt -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
-                    </svg>
-                  </div>
+                    <div class="flex">
+                    <div class="flex-shrink-0">
+                        <div class="flex items-center justify-center h-12 w-12 rounded-mdtext-white">
+                            <img src="./instagram.svg" alt=""><br>
+                        </div>
+                    </div>
+                    <div class="ml-4">
+                        <dt class="text-lg leading-6 font-medium">
+                        Instagram
+                        </dt>
+                        <span>
+                            Entrez dans mon intimité en vous abonnant à mon 
+                             <a href="https://www.instagram.com/xavierdang/" target="_blank" class="mt-2 text-base h-12 w-12 text-indigo-600">
+                                compte Instagram.
+                            </a>
+                            Vous pourrez voir mes story où je fais des pâtes ou encore mes têtes after sex avec ma femme.
+                        </span>
+                    </div>
+                    </div>
+                </dl>
                 </div>
-                <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    Ecoutez mes sons sur soudcloud
-                  </dt>
-                  <a href="https://soundcloud.com/xavierdang" target="_blank" class="mt-2 text-base text-gray-500">
-                    https://soundcloud.com/xavierdang
-                  </a>
-                </div>
-              </div>
-
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: annotation -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    Twitter et Instagrams
-                  </dt>
-                  <a href="https://www.instagram.com/xavierdang/" target="_blank" class="mt-2 text-base text-gray-500">
-                    https://www.instagram.com/xavierdang/<br>
-                    https://twitter.com/mistermv
-                  </a>
-                </div>
-              </div>
-            </dl>
-          </div>
+            </div>
+            <div class="mt-4 max-w-2xl lg:mx-auto flex">
+                    <div class="flex-shrink-0">
+                        <div class="flex items-center justify-center h-12 w-12 rounded-md text-white">
+                            <img src="./twitter.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="ml-4">
+                        <dt class="text-lg leading-6 font-medium">
+                        Twitter
+                        </dt>
+                        <span>
+                            Avoir de venir voir l'immensité de ma bêtise et voir ce que je partage n'hésitez pas à aller sur 
+                            <a href="https://twitter.com/mistermv" target="_blank" class="mt-2 text-base h-12 w-12 text-indigo-600">
+                                mon Twitter.
+                            </a>
+                            C'est en grande partie grâce à ça que l'incroyable album va voir le jour.
+                        </span>
+                    </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
