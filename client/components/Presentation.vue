580dea3a0f2a3d80acce2ff83789ddae166e44f5
AlexandrePdv <48692213+AlexandrePdv@users.noreply.github.com>
2020-11-23 03:59:54 +0100

    Presentation terminé

--- client/components/Presentation.vue
+++ client/components/Presentation.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <div>
-      <div class="py-12 bg-white">
+      <div class="min-h-screen min-w-screen py-16 bg-gray-900 text-gray-300">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="lg:text-center">
-            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl">
               Mais qui est MisterMV ?
             </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <img class="mt-4 max-w-2xl lg:mx-auto" src="https://pbs.twimg.com/profile_images/1250788764131233794/iL0mbfDM.jpg" alt="">
+            <p class="mt-4 max-w-2xl text-xl lg:mx-auto">
               Xavier Dang, né le 26 août 1980 et plus connu sous le pseudonyme mistermv, est un streamer, vidéaste, animateur et musicien français spécialisé dans le jeu vidéo et le speedrun. Il est dans un premier temps musicien à partir de la fin des années 1990, travaillant notamment dans la musique électronique pour des jeux vidéo, avant de se reconvertir à plein temps au début des années 2010 dans le streaming.
             </p>
           </div>
@@ -15,93 +14,51 @@
           <div class="mt-10">
             <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
               <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                    </svg>
-                  </div>
-                </div>
                 <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    TopAchat est partenaire de mon stream !
+                  <div class="flex">
+                    <a href="http://www.topachat.com/" target="blank"><img src="https://panels-images.twitch.tv/panel-28575692-image-936bb43a-cbd2-4547-9bb5-cc5b4993f750" alt=""></a>
+                  </div>
+                  <dt class="text-lg leading-6 font-medium">
+                    <a href="http://www.topachat.com/" target="blank">TopAchat</a>
+                      est partenaire de mon stream !
                   </dt>
-                  <dd class="mt-2 text-base text-gray-500">
+                  <dd class="mt-2 text-base">
                     N'hésitez pas à passer chez eux pour tos vos besoins en composants informatiques, ou essayez leur fameux outil ConfigoMatic pour créer votre propre config ou voir celles des autres utilisateurs !
                   </dd>
                 </div>
               </div>
 
               <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: scale -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                </div>
                 <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
+                  <div class="flex">
+                    <a href="http://bit.ly/mvGOG" target="blank"><img src="https://panels-images.twitch.tv/panel-28575692-image-e84f8278-1d25-42f9-a176-718b7474ae6c" alt=""></a>
+                  </div>
+                  <dt class="text-lg leading-6 font-medium">
                     Je suis ambassadeur français pour GOG !
                   </dt>
-                  <dd class="mt-2 text-base text-gray-500">
+                  <dd class="mt-2 text-base">
                     GOG propose plein de jeux indé, rétro & nouveautés à de bons prix et surtout sans DRM ! Chaque vente effectuée ou compte créé via mon lien d'affiliation m'aide également ! Cliquez sur le bouton pour visiter le site via ma page perso !
                   </dd>
                 </div>
               </div>
 
               <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: lightning-bolt -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                    </svg>
-                  </div>
-                </div>
                 <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    Mon matériel
+                  <dt class="text-lg leading-6 font-medium">
+                    La boutique des goodies ! Qui veut des goodiiiiiiies ? Cliquez sur l'image ! 
                   </dt>
-                  <dd class="mt-2 text-base text-gray-500">
-                    PC<br>
-                    Boitier : Cooler Master Cosmos C700P<br>
-                    Processeur : Intel Core i9-7920X<br>
-                    Carte Mère : Asus ROG STRIX X299-E GAMING<br>
-                    Mémoire : DDR4 G.Skill Trident Z RGB - 4 x 8 Go<br>
-                    Carte Graphique : EVGA GeForce GTX 1080 Ti<br>
-                    SSD M2 1 : Samsung 970 EVO Plus NVMe M.2<br>
-                    SSD M2 2 : CORSAIR Force MP510 1920 GB NVMe PCIe Gen3 x4 M.2<br>
-                    SSD Interne : Samsung Série 850 EVO 1 To<br>
-                    HDD interne : Seagate Barracuda Pro 4To 7200tr/m<br>
-                    Lumière : Elgato Key Light x2<br>
-                    SON<br>
-                    Carte Son : GoXLR<br>
-                    Carte Son/Table de Mixage (alternative) : Yamaha MG10XU<br>
-                    Micro : Shure SM7B<br>
-                    Préamplificateur Micro : Cloud microphones Cloudlifter CL-1<br>
-                    Casque : Audio Technica ATH-M50X<br>
-                    Contrôleur MIDI : Native Instruments Komplete Kontrol S25<br>
-                    Contrôleur MIDI 2 : Native Instruments Komplete Kontrol S61 MK2<br>
-                  </dd>
+                  <div class="flex">
+                    <a href="https://quiveutdesgoodies.com/" target="_blank"><img src="https://panels-images.twitch.tv/panel-28575692-image-7dc77172-3f55-4e9a-8a2d-6d9c9de0fcb9" alt=""></a>
+                  </div>
                 </div>
               </div>
 
               <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <!-- Heroicon name: annotation -->
-                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                    </svg>
-                  </div>
-                </div>
                 <div class="ml-4">
-                  <dt class="text-lg leading-6 font-medium text-gray-900">
-                    SpeeDons
-                  </dt>
-                  <dd class="mt-2 text-base text-gray-500">
+                  <div class="flex">
+                    <a href="http://www.speedons.fr/" target="blank"><img src="https://panels-images.twitch.tv/panel-28575692-image-542ee8b3-3c2d-4acf-a151-3e11fa8347fe" alt=""></a>
+                  </div>
+                  <dd class="mt-2 text-base">
                     SpeeDons est un marathon de speedrun caritatif de 55 heures diffusé sur Twitch & levant des dons pour Médecins Du Monde. L'événement a été reporté à cause des mesures de confinement liées au covid-19. Plus d'infos dans les prochaines semaines à venir.
                   </dd>
                 </div>
@@ -110,8 +67,6 @@
           </div>
         </div>
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
