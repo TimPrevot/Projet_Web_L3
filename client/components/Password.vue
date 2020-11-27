1fbc1194daed3267c5f662522d228137226d6050
deadbang3r <locopok33185@gmail.com>
2020-11-27 15:39:59 +0100

    Rétablissement de l'envoi de mails + suppression de la div associée

--- client/components/Password.vue
+++ client/components/Password.vue
@@ -5,25 +5,22 @@
         <h3 class="pt-4 mb-2 text-2xl">Vous avez oublié votre mot de passe?</h3>
       </div>
 
-      <div v-if="this.email_sent">
-        <h2>Nous vous avons envoyé un email pour réinitialiser votre mot de passe</h2>
-      </div>
-
       <form class="px-8 pt-6 pb-8 mb-4 rounded" @submit.prevent="sendEmail">
         <div class="mb-4">
-          <label class="block mb-2 text-sm font-bold text-gray-700">
+          <label for="email" class="block mb-2 text-sm font-bold text-gray-700">
             E-mail
           </label>
           <input
               class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              type="email"
+              id="email"
+              type="email" name="user_email"
               placeholder="E-mail"
               required
           />
         </div>
-        <div class="mb-6 text-center" v-if="!this.email_sent">
-          <button @click="setEmailToTrue"
-              type="submit"
+        <div class="mb-6 text-center">
+          <button
+              type="submit" value="Send"
               class="w-full px-4 py-2 font-bold text-white bg-indigo-600 rounded-full hover:bg-indigo-700 focus:outline-none focus:shadow-outline"
           >Réinitialiser le mot de passe
           </button>
@@ -57,13 +54,9 @@ module.exports = {
       emailjs.sendForm('recover_password', 'mot_de_passe_oublie', e.target, 'user_coH8k9D5xfQORmgGGdVd0')
         .then((result) => {
           console.log('Email sent successfully', result.status, result.text)
-          console.log('email sent: ', this.email_sent)
         }, (error) => {
           console.log('Failed', error)
         })
-    },
-    setEmailToTrue() {
-      this.email_sent = true
     }
   }
 }
